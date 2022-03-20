b90d354afc2a259987c17b7eabe385270657dbbd
LAKSHMIPM75 <101916649+LAKSHMIPM75@users.noreply.github.com>
2022-03-20 22:59:32 +0530

    File is added

---
+++ Account.java
@@ -0,0 +1,12 @@
+
+public class Account {
+	void account(String name, float deposit, float withdraw, float checkbalance)
+	{
+		System.out.println(name+" "+deposit+" "+withdraw+" "+checkbalance);		
+	}
+	
+	
+	
+
+}
+
