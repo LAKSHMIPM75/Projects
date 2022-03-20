46b96984f9322abd2fcb15322bae0c956e6f65ce
LAKSHMIPM75 <101916649+LAKSHMIPM75@users.noreply.github.com>
2022-03-20 23:03:52 +0530

    File is added

---
+++ Reverse_Number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Reverse_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int orgNum = in.nextInt();
+        for(int i=5;i>=1;i--) {
+        	System.out.print(i);
+        }
+        
+       
+        System.out.println();
+
+	}
+
+}
